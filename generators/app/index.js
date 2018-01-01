928b433c45953dd8b8f8bfd592395c556d18daec
cexoso <285516193@qq.com>
2018-01-01 12:47:54 +0800

    结构完成

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,41 +2,67 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const askName = require('inquirer-npm-name');
+const path = require('path');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
+  constructor(...o) {
+    super(...o);
+    this.props = {};
+  }
   prompting() {
-    // Have Yeoman greet the user.
     this.log(
       yosay('Welcome to the peachy ' + chalk.red('generator-yunke-react') + ' generator!')
     );
-
-    const prompts = [
+    return askName(
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+        name: 'name',
+        message: 'Your Project name',
+        default: path.basename(process.cwd())
+      },
+      this
+    ).then(props => {
+      this.props.name = props.name;
     });
   }
-
-  writing() {
+  default() {
+    if (path.basename(this.destinationPath()) !== this.props.name) {
+      this.log(
+        `Your project must be inside a folder named 
+        ${this.props.name}
+        \nI'll automatically create this folder.`
+      );
+      mkdirp(this.props.name);
+      this.destinationRoot(this.destinationPath(this.props.name));
+      this.composeWith(require.resolve('generator-node/generators/app'), {
+        boilerplate: false,
+        cli: false,
+        travis: false
+      });
+    }
+  }
+  _writePackage() {
+    const pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
+    pkg.scripts = pkg.scripts || {};
+    pkg.scripts.start = 'webpack-dev-server';
+    this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+  }
+  _writeReactTpl() {
+    this.fs.copy(this.templatePath('index.js'), this.destinationPath('index.js'));
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
     );
   }
+  Writing() {
+    this._writePackage();
+    this._writeReactTpl();
+  }
 
-  install() {
-    this.installDependencies({
-      npm: false,
-      bower: false,
-      yarn: true
-    });
+  Install() {
+    if (this.options.skipInstall) {
+      this.installDependencies({ bower: false });
+    }
   }
 };
