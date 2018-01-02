2a82178e126408bb46a3445def50b138e28b6b9f
熊杰 <285516193@qq.com>
2018-01-02 11:23:58 +0800

    添加git功能

---
+++ __tests__/git.js
@@ -0,0 +1,26 @@
+'use strict';
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+describe('node:git', () => {
+  it('creates the git config files and init the repository', () => {
+    return helpers.run(require.resolve('../generators/git')).then(() => {
+      assert.file('.gitignore');
+      assert.file('.gitattributes');
+      assert.file('.git');
+    });
+  });
+
+  it('respects --generate-into option', () => {
+    return helpers
+      .run(require.resolve('../generators/git'))
+      .withOptions({
+        generateInto: 'other/'
+      })
+      .then(() => {
+        assert.file('other/.gitignore');
+        assert.file('other/.gitattributes');
+        assert.file('other/.git');
+      });
+  });
+});
